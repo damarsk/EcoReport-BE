06d90b7897f475b8e8a11b0947fb764efabd0795
Damar S.K <damarsyahada12@gmail.com>
2026-06-03 17:34:11 +0700

    feat: setup cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import authRoutes from "./routes/auth.route";
 import categoryRoutes from "./routes/category.route";
 import profileRoutes from "./routes/profile.route";
@@ -7,6 +8,13 @@ import { errorHandler } from "./middlewares/error.middleware";
 import { swaggerDocs } from "./docs/swagger";
 
 const app = express();
+const corsOptions = {
+  origin: "*",
+  methods: "GET,PUT,POST,DELETE",
+  allowedHeaders: "Content-Type, Authorization",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 if (process.env.ENVIRONMENT === "DEV") {
